dbc67e54acfb7d1ae6169194b0f4822b97ece981
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-08-14 16:08:02 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,10 @@
-<div class="down-button" :class="transformButton(index)">
+<q-btn
+  flat
+  round
+  color="primary"
+  padding="none"
+  size="md"
+  :icon="activeBanner === category.id ? 'mdi-chevron-up' : 'mdi-chevron-down'" <!-- Меняем иконку -->
+  :class="{'transform-button': activeBanner === category.id}" <!-- Добавляем класс при активном баннере -->
+  @click="toggleBanner(category.id)" <!-- Обработчик клика -->
+/>
