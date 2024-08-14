a1bf3223b43ce77fcf5c6870a1327bc066b73163
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-08-15 01:32:28 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,60 +1,23 @@
-import axios from "axios";
-import { ref } from "vue";
+<q-route-tab
+  name="7207783892693892797"
+  label="Профессиональные навыки"
+  :to="{ path: `/content-items/${7207783892693892797}` }"
+/>
 
-const BACKEND_URL = "https://als-wt/pp/Ext5/extjs_json_collection_data.html";
+<q-route-tab
+  name="7207784069377055908"
+  label="Личная эффективность"
+  :to="{ path: `/content-items/${7207784069377055908}` }"
+/>
 
-// Предопределенные категории
-const CATEGORIES = [
-  { id: "7207783892693892797", title: "Профессиональные навыки" },
-  { id: "7207784069377055908", title: "Личная эффективность и развитие" },
-  { id: "7207784147986504834", title: "Вводные курсы (инструктажи)" },
-  { id: "7313878051766677160", title: "Управление эффективностью деятельности (УЭД)" },
-];
+<q-route-tab
+  name="7207784147986504834"
+  label="Вводные курсы"
+  :to="{ path: `/content-items/${7207784147986504834}` }"
+/>
 
-export default {
-  setup() {
-    return {
-      currentCategoryTitle: ref(""),
-    };
-  },
-  data() {
-    return {
-      category: {},
-    };
-  },
-  methods: {
-    async fetchCourseData() {
-      try {
-        const params = {
-          collection_code: "vtbl_quasar_course_data",
-          parameters: "course_id=" + this.$route.params.id.substring(1),
-        };
-
-        const response = await axios.post(
-          BACKEND_URL,
-          new URLSearchParams(params).toString()
-        );
-
-        // Получаем данные курса
-        const courseData = response.data.results[0];
-        this.category = courseData;
-
-        // Ищем категорию по parentCategory
-        const categoryId = courseData.parentCategory[0];
-        const categoryName = CATEGORIES.find((cat) => cat.id === categoryId);
-
-        if (categoryName) {
-          this.currentCategoryTitle = categoryName.title;
-        } else {
-          this.currentCategoryTitle = "Категория не найдена";
-        }
-      } catch (error) {
-        console.error("Ошибка загрузки курса", error);
-      }
-    },
-  },
-  mounted() {
-    this.fetchCourseData();
-  },
-};
-
+<q-route-tab
+  name="7313878051766677160"
+  label="УЭД"
+  :to="{ path: `/content-items/${7313878051766677160}` }"
+/>
