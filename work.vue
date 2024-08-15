173cf09a598ad05e5ddd95f73eca30f2f571180e
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-08-15 15:45:55 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,36 +1,13 @@
 methods: {
   navigateToTab(tabId) {
     const path = `/content-items/${tabId}`;
-    this.$router.push(path);
+    
+    // Осуществляем переход на новый маршрут
+    this.$router.push(path).then(() => {
+      // После успешного перехода перезагружаем страницу
+      window.location.reload();
+    }).catch(err => {
+      console.error('Error navigating to tab:', err);
+    });
   }
 }
-
-<q-tabs
-  v-model="tab"
-  class="text-primary q-mb-sm"
-  active-color="secondary"
-  align="justify"
-  narrow-indicator
->
-  <q-tab 
-    name="7207783892693892797" 
-    label="Профессиональные навыки"
-    @click="navigateToTab('7207783892693892797')"
-  />
-  <q-tab 
-    name="7207784069377055908" 
-    label="Личная эффективность"
-    @click="navigateToTab('7207784069377055908')"
-  />
-  <q-tab 
-    name="7207784147986504834" 
-    label="Вводные курсы"
-    @click="navigateToTab('7207784147986504834')"
-  />
-  <q-tab 
-    name="7313878051766677160" 
-    label="УЭД"
-    @click="navigateToTab('7313878051766677160')"
-  />
-</q-tabs>
-
