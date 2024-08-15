c49dec2f9449ed02892415c63ec15480ef971ae3
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-08-15 11:00:15 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,12 +1,1 @@
-methods: {
-  handleTabClick(path) {
-    if (path.startsWith('http')) {
-      // Переход на внешний URL
-      window.location.href = path;
-    } else {
-      // Переход внутри приложения
-      this.$router.push(path);
-    }
-  },
-  // ... остальные методы
-}
+
