f92ffbd99d2debeb5dc4ed4fd6b791ddc6abec91
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-08-15 14:08:53 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,11 +1,1 @@
-this.$router.push(path).catch(err => {
-  if (err.name !== 'NavigationDuplicated') {
-    throw err;
-  }
-});
-
-
-this.$router.push(path).then(() => {
-  this.$router.go(0);
-});
 
