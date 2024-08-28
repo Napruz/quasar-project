f0f98708f1f3231f4065f3db30259f7011fa66a3
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-08-28 14:00:08 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,16 +1,1 @@
-<style>
-.hover-effect:hover {
-  background-color: rgba(0, 0, 0, 0.1); 
-}
-</style>
 
-onMounted(() => {
-      fetchMenuLinks();
-      fetchUsersInfo();
-
-      // Находим элемент и добавляем ему класс hover-effect
-      const targetElement = document.querySelector(".q-item.q-item-type.row.no-wrap");
-      if (targetElement) {
-        targetElement.classList.add("hover-effect");
-      }
-    });
