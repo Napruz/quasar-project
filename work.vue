f826db8d287e3db0a3a099a21f887b9f539e96fe
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-09-12 13:31:57 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,15 @@
+// store/categoryid.js
+import { defineStore } from 'pinia';
+
+export const useCategoryStore = defineStore('category', {
+  state: () => ({
+    currentCategoryId: null, // Здесь будет храниться ID текущей категории
+  }),
+
+  actions: {
+    setCategoryId(categoryId) {
+      this.currentCategoryId = categoryId; // Обновляем ID категории
+    },
+  },
+});
 
