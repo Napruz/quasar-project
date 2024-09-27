9523b4c68de611806c8727304e1b81c15ba3a014
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-09-27 12:02:33 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,15 +1,1 @@
-<ul class="courses-themes-list">
-  <li
-    v-for="(course, index) in category.courses.filter(c => c !== '')"
-    :key="index"
-    class="list-item"
-  >
-    <div class="themes-arrow-items row">
-      <div class="q-ml-xs">
-        <p class="main-themes-description text-black q-mb-xs">
-          {{ course }}
-        </p>
-      </div>
-    </div>
-  </li>
-</ul>
+
