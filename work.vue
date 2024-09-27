4d6dabb99107fcf6996273db665ccecabc5466f6
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-09-27 11:44:46 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,53 +1,15 @@
-<!-- CatalogPage.vue -->
-<template>
-  <div class="main-container row justify-between">
-    <div class="content-container">
-      <div class="new-card">
-        <q-card class="my-card">
-          <q-img class="card-header-bg-image" src="images/card-header-panel.png">
-            <div v-if="category.state === 'inProgress'" class="course-state in-progress">
-              <q-img src="icons/in-progress-state.svg" />
-            </div>
-          </q-img>
-        </q-card>
+<ul class="courses-themes-list">
+  <li
+    v-for="(course, index) in category.courses.filter(c => c !== '')"
+    :key="index"
+    class="list-item"
+  >
+    <div class="themes-arrow-items row">
+      <div class="q-ml-xs">
+        <p class="main-themes-description text-black q-mb-xs">
+          {{ course }}
+        </p>
       </div>
     </div>
-  </div>
-</template>
-
-<script>
-import { useCategoryStore } from '@/store/categoryid';
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      category: {}, // Данные курса
-      courses: [] // Курсы в категории
-    };
-  },
-  methods: {
-    fetchCourseData() {
-      const categoryStore = useCategoryStore(); // Получаем доступ к хранилищу
-      const categoryId = categoryStore.currentCategoryId; // Получаем ID категории
-
-      // Выполняем запрос с использованием полученного ID
-      const params = {
-        collection_code: 'vtbl_quasar_courses_list',
-        parameters: `category_id=${categoryId}`,
-      };
-
-      axios.post('https://als-wt/pp/Ext5/extjs_json_collection_data.html', new URLSearchParams(params).toString())
-        .then(response => {
-          this.courses = response.data.results;
-        })
-        .catch(error => {
-          console.error('Ошибка загрузки данных курса:', error);
-        });
-    }
-  },
-  mounted() {
-    this.fetchCourseData(); // Получаем данные при монтировании
-  }
-};
-</script>
+  </li>
+</ul>
