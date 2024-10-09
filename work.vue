fa578f9293cff82495417e9bac9d5d6ce44607c0
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2024-10-09 15:17:21 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,22 @@
+const mutObserver = async () => {
+  let observer = new MutationObserver(function (mutations) {
+    console.log(mutations);
+
+    mutations.forEach(function (mutationRecord) {
+      const wtBodyElement = document.getElementById('wt-body');
+      
+      if (wtBodyElement && mutationRecord.target.style.paddingLeft) {
+        wtBodyElement.style.paddingLeft = mutationRecord.target.style.paddingLeft;
+      }
+    });
+  });
+
+  const pageContainer = document.getElementsByClassName('q-page-container')[0];
+  
+  if (pageContainer) {
+    observer.observe(pageContainer, { attributes: true, attributeFilter: ['style'] });
+  } else {
+    console.error("Не найден элемент с классом 'q-page-container'");
+  }
+};
 
