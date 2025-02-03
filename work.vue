9efa7c49a44aa629641d8ec3e181a79db61f9d0f
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2025-02-03 13:33:35 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,13 +1,1 @@
-mounted() {
-  console.log('Component mounted'); // Убедитесь, что метод вызван.
-  this.fetchEventData();
 
-  nextTick(() => {
-    const buttonMargin = document.querySelectorAll('.block');
-    console.log('Elements found:', buttonMargin.length); // Убедитесь, что элементы найдены.
-    buttonMargin.forEach((el) => {
-      el.style.marginTop = "2px";
-      console.log('button', el); // Выведите элемент для проверки.
-    });
-  });
-},
