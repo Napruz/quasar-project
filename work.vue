c8c3521943afd114a4c887b12ee35cd95742f963
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2025-02-27 12:15:43 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,12 +1,30 @@
-methods: {
-  printPage() {
-    const drawer = document.querySelector('.q-drawer');
-    if (drawer) drawer.style.display = 'none';
+<script setup>
+import { nextTick } from 'vue'
 
-    window.print();
+const printPage = async () => {
+  await nextTick() // Дожидаемся завершения обновлений DOM
 
-    setTimeout(() => {
-      if (drawer) drawer.style.display = '';
-    }, 1000);
-  }
+  setTimeout(() => {
+    window.print()
+  }, 300) // Можно увеличить задержку, если нужно
 }
+</script>
+
+<template>
+  <div class="print-page-container">
+    <q-btn
+      color="grey"
+      icon="print"
+      label="Печать"
+      class="print-page-btn"
+      size="sm"
+      @click="printPage"
+    />
+  </div>
+</template>
+
+<style>
+.print-page-btn {
+  position: absolute;
+}
+</style>
