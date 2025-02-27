8484864e5106050438bbc0de675b11e167e4d689
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2025-02-27 12:53:20 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,74 @@
+<script>
+import { ref, onMounted, nextTick } from "vue";
+import axios from "axios";
+
+const BACKEND_URL =
+  window.location.origin + 
+const BACKEND_POST_URL =
+  window.location.origin + 
+
+export default {
+  setup() {
+
+
+    const changeStepperStyle = () => {
+      document.querySelectorAll(".q-stepper__tab").forEach((el) => {
+        el.style.paddingLeft = "5px";
+        el.style.paddingRight = "5px";
+      });
+    };
+
+    const changeFontSizeStyle = () => {
+      document.querySelectorAll(".q-stepper__title").forEach((el) => {
+        el.style.fontSize = "14px";
+      });
+    };
+
+    const changeAnotherFontSizeStyle = () => {
+      document.querySelectorAll(".q-stepper__caption").forEach((el) => {
+        el.style.fontSize = "12px";
+      });
+    };
+
+    onMounted(() => {
+      fetchCabinetData();
+      fetchMaterialsData();
+      fetchCommentListData();
+      fetchUsersData();
+      fetchCoworkers();
+
+      const observer = new MutationObserver(async (mutationsList) => {
+        await nextTick();
+
+        // Выполняем стилизацию при любом изменении DOM
+        mutationsList.forEach(() => {
+          changeStepperStyle();
+          changeFontSizeStyle();
+          changeAnotherFontSizeStyle();
+        });
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+        attributes: true, // Отслеживание изменений атрибутов
+      });
+    });
+
+    return {
+      activeTab,
+      toastVisible,
+      toastMessage,
+      printPage,
+      cabinetData,
+      usersData,
+      materialsData,
+      commentListData,
+      taskData,
+      isCoworkersModalOpen,
+      coworkersData,
+    };
+  },
+};
+</script>
 
