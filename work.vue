a34601d04ffbc88c55184077f93bd25576e9fa22
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2025-11-17 15:27:04 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,55 +1,55 @@
 // Проверяем, есть ли ID редактируемой заявки
-var sReqID = getFormField("request_id", "");
+var sReqID = OptInt(getFormField("request_id", getFormFieldDefault("request_id", "step_0")), 0);
 
-if (sReqID && sReqID != "") {
+if (sReqID != 0) {
     var oReqDoc = tools.open_doc(sReqID);
     var oReqDocTE = oReqDoc.TopElem;
 
     // Подставляем значения в поля формы
     for (var i = 0; i < oForm.form_fields.length; i++) {
-        var field = oForm.form_fields[i];
-        switch(field.name){
+        var fField = oForm.form_fields[i]; // переименовано
+        switch(fField.name){
             case "name":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_name").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_name").value;
                 break;
             case "subdivision_id":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_subdivision_id").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_subdivision_id").value;
                 break;
             case "subdivision_text":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_subdivision_name").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_subdivision_name").value;
                 break;
             case "sheCode":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_cod_he").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_cod_he").value;
                 break;
             case "function":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_function").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_function").value;
                 break;
             case "vacancyReason":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_cause").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_cause").value;
                 break;
             case "workPlace":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_place").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_place").value;
                 break;
             case "salary":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_salary").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_salary").value;
                 break;
             case "bonusSystem":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_bonus_system").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_bonus_system").value;
                 break;
             case "gasCost":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_gas_cost").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_gas_cost").value;
                 break;
             case "jobSharing":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_job_sharing").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_job_sharing").value;
                 break;
             case "hiringPurpose":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_cupor_rationale").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_vacancy_cupor_rationale").value;
                 break;
             case "duties":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_candidate_function").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_candidate_function").value;
                 break;
             case "requirements":
-                field.value = oReqDocTE.custom_elems.ObtainChildByKey("f_candidate_quality").value;
+                fField.value = oReqDocTE.custom_elems.ObtainChildByKey("f_candidate_quality").value;
                 break;
         }
     }
