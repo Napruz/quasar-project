18b646c34d4fae7f8538ba3455c30207d649f7f9
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2025-11-17 16:36:45 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,15 @@
+var sReqID = getFormField("request_id","");
+var oReqDoc;
+
+if (sReqID != "") {
+    oReqDoc = tools.open_doc(sReqID); // открываем существующую заявку
+} else {
+    oReqDoc = tools.new_doc_by_name("request"); // только если реально нет ID
+    oReqDoc.BindToDb();
+}
+
+var oReqDocTE = oReqDoc.TopElem;
+oReqDocTE.person_id = oOptions.id;
+
+// далее заполняем поля, как раньше
 
