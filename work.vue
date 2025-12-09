03f45885e557f1dbb313db9d477fea14f971b481
Rupert Pumpkin <149478211+Napruz@users.noreply.github.com>
2025-12-09 14:33:31 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,6 +1,37 @@
-watch(collaboratorDialogOpen, (val) => {
-  if (val === false) {
-    // диалог закрыли (клик снаружи, esc, кнопка)
-    resetCollaboratorSearch();
+<q-dialog
+  v-model="collaboratorDialogOpen"
+  @hide="onCollaboratorDialogHide"
+>
+  ...
+</q-dialog>
+
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const collaboratorDialogOpen = ref(false);
+    const collaboratorSearch = ref('');
+    const collaboratorPage = ref(1);
+    const collaboratorListData = ref([]);
+
+    const resetCollaboratorSearch = () => {
+      collaboratorSearch.value = '';
+      collaboratorPage.value = 1;
+      collaboratorListData.value = [];
+    };
+
+    const onCollaboratorDialogHide = () => {
+      // вызывется всегда при закрытии (клик вне, esc, programmatic toggle)
+      resetCollaboratorSearch();
+    };
+
+    return {
+      collaboratorDialogOpen,
+      collaboratorSearch,
+      collaboratorPage,
+      collaboratorListData,
+      onCollaboratorDialogHide,
+    };
   }
-});
+};
+
